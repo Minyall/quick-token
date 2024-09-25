918dd70aa94eff97138b8b2b48ccfb5c2141891f
James Allen-Robertson <minyall@gmail.com>
2024-09-25 22:49:59 +0100

    rename

--- __init__.py
+++ __init__.py
@@ -1,1 +1,1 @@
-from quick_tokenize import tokenize_span, bigram_process, spacy_stream, tokenize, quick_token, load_quick_tokens
+from tokenize_it import tokenize_span, bigram_process, spacy_stream, tokenize, quick_token, load_quick_tokens
